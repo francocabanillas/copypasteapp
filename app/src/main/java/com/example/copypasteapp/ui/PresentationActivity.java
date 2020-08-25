8962b9b7f0671b9b4e06dfdac9da9f748c60820f
Franco Cabanillas <cabanillas.cf@gmail.com>
2020-08-24 22:50:23 -0500

    Primera subida
    Carga inicial de todas las actividades en blanco

---
+++ app/src/main/java/com/example/copypasteapp/ui/PresentationActivity.java
@@ -0,0 +1,16 @@
+package com.example.copypasteapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.copypasteapp.R;
+
+public class PresentationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_presentation);
+    }
+}
