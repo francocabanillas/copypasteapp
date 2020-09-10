9bf975a747b7252dfcf736e692e35df7c7e0b82d
Franco Cabanillas <cabanillas.cf@gmail.com>
2020-09-10 00:12:27 -0500

    Consumo de servicios
    Login y registro de cliente

--- app/src/main/java/com/example/copypasteapp/ui/LoginActivity.java
+++ app/src/main/java/com/example/copypasteapp/ui/LoginActivity.java
@@ -4,9 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.copypasteapp.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,10 +37,6 @@ public class LoginActivity extends AppCompatActivity {
         getSupportActionBar().hide();
     }
 
-    public void ingresar(View view){
-        startActivity(new Intent(this, MainActivity.class));
-    }
-
     public void registrar(View view){
         startActivity(new Intent(this, RegisterActivity.class));
     }
@@ -28,4 +44,60 @@ public class LoginActivity extends AppCompatActivity {
     public void restablecer(View view){
         startActivity(new Intent(this, ResetpasswordActivity.class));
     }
+
+    public void login(View v){
+        final Intent intent = new Intent(this, MainActivity.class);
+        final EditText username = (EditText)findViewById(R.id.username);
+        final EditText password = (EditText)findViewById(R.id.password);
+
+        String tUsername = username.getText().toString();
+        String tPassword = username.getText().toString();
+        String url = "http://copypaste.atwebpages.com/index.php/login";
+
+        StringRequest stringRequest= new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONArray jsonArray = new JSONArray(response);
+                    Log.i("onResponse", jsonArray.toString());
+                    if (jsonArray.length()>0){
+                        startActivity(intent);
+                    }else
+                    {
+                        Toast toast = Toast.makeText(LoginActivity.this,"Usuario y/o contraseña incorrectos", Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                        toast.show();
+                    };
+
+                } catch (JSONException e) {
+                    Log.i("JSONException", e.getMessage());
+                }
+            }
+        },
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.i("onErrorResponse", error.toString());
+                }
+            }
+        ){
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap();
+                params.put("correo", username.getText().toString());
+                params.put("clave", password.getText().toString());
+                return params;
+            }
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("Content-Type","application/x-www-form-urlencoded");
+                return params;
+            }
+        };
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+    }
+
 }
--- app/src/main/java/com/example/copypasteapp/ui/RegisterActivity.java
+++ app/src/main/java/com/example/copypasteapp/ui/RegisterActivity.java
@@ -2,9 +2,28 @@ package com.example.copypasteapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.copypasteapp.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -14,4 +33,101 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         getSupportActionBar().hide();
     }
+
+    public void registrar(View v){
+
+        final Intent intent = new Intent(this, LoginActivity.class);
+
+        final EditText clientenombre = (EditText)findViewById(R.id.clientenombre);
+        final EditText correo = (EditText)findViewById(R.id.correo);
+        final EditText celular = (EditText)findViewById(R.id.celular);
+        final EditText clave = (EditText)findViewById(R.id.clave);
+        final EditText clave2 = (EditText)findViewById(R.id.clave2);
+
+        String url = "http://copypaste.atwebpages.com/index.php/cliente";
+
+        if (clientenombre.getText().toString().length()==0){
+            Toast toast = Toast.makeText(RegisterActivity.this,"Ingrese su nombre", Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+        }
+        else if (correo.getText().toString().length()==0){
+            Toast toast = Toast.makeText(RegisterActivity.this,"Ingrese su correo", Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+        }
+        else if (celular.getText().toString().length()==0){
+            Toast toast = Toast.makeText(RegisterActivity.this,"Ingrese su celular", Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+        }
+        else if (clave.getText().toString().length()==0){
+            Toast toast = Toast.makeText(RegisterActivity.this,"Ingrese su clave", Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+        }
+        else if (clave2.getText().toString().length()==0){
+            Toast toast = Toast.makeText(RegisterActivity.this,"Repita su clave", Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+        }
+        else if (!clave.getText().toString().equals(clave2.getText().toString())) {
+
+            Toast toast = Toast.makeText(RegisterActivity.this,"La contraseña ingresada no coincide", Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+
+        }else{
+
+            StringRequest stringRequest= new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        JSONArray jsonArray = new JSONArray(response);
+                        Log.i("onResponse", jsonArray.toString());
+
+
+                            Toast toast = Toast.makeText(RegisterActivity.this,"Ha sido registrado", Toast.LENGTH_LONG);
+                            toast.setGravity(Gravity.CENTER, 0, 0);
+                            toast.show();
+                            startActivity(intent);
+
+                    } catch (JSONException e) {
+                        Log.i("JSONException", e.getMessage());
+                    }
+                }
+            },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.i("onErrorResponse", error.toString());
+                        }
+                    }
+            ){
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> params = new HashMap();
+                    params.put("cliente_nombre", clientenombre.getText().toString());
+                    params.put("correo", correo.getText().toString());
+                    params.put("celular", celular.getText().toString());
+                    params.put("clave", clave.getText().toString());
+                    return params;
+                }
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String,String> params = new HashMap<String, String>();
+                    params.put("Content-Type","application/x-www-form-urlencoded");
+                    return params;
+                }
+            };
+            RequestQueue requestQueue= Volley.newRequestQueue(this);
+            requestQueue.add(stringRequest);
+
+        }
+
+
+
+
+    }
+
 }
